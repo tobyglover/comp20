bfbf4cc70ca2b8f19f921a293f0451f7bd9094c9
Toby Glover <william.glover@tufts.edu>
2016-10-03 18:56:29 -0400

    Added user location

--- mbta/scripts.js
+++ mbta/scripts.js
@@ -1,5 +1,6 @@
 var map;
-var userPos = {lat: 0, lng: 0};
+var userMarker;
+var userPos = {lat: -1, lng: -1};
 
 function init() {
 	map = new google.maps.Map(document.getElementById('map'), {
@@ -7,4 +8,29 @@ function init() {
           	zoom: 13
         });
 
+	getUserLocation();
+}
+
+function getUserLocation() {
+	if ("geolocation" in navigator) {/* geolocation is available */
+  		navigator.geolocation.getCurrentPosition(function(position) {
+  			userPos.lat = position.coords.latitude;
+  			userPos.lng = position.coords.longitude;
+  			populateUserMarker()
+  		}, function (err) {
+  			console.warn('ERROR(' + err.code + '): ' + err.message);
+  			alert("Unable to get current location");
+  		})
+	} else {
+		alert("Geolocation is not supported by your web browser.");
+	}
+}
+
+function populateUserMarker() {
+	userMarker = new google.maps.Marker({
+    	position: userPos,
+    	map: map
+  	});
+
+  	map.panTo(userPos);
 }
