5d9bda276397a8fa90fcf5347a35c5a60e929a4e
Toby Glover <william.glover@tufts.edu>
2016-10-04 17:35:22 -0400

    changed data structure for stations to make data more accessible

--- mbta/scripts.js
+++ mbta/scripts.js
@@ -1,49 +1,31 @@
 var map;
 var userMarker;
 var userPos = {lat: -1, lng: -1};
-var stationList = [{name:"Alewife", coords:{lat:42.395428, lng:-71.142483}},
-				   {name:"Davis", coords:{lat:42.39674, lng:-71.121815}},
-				   {name:"Porter Square", coords:{lat:42.3884, lng:-71.11914899999999}},
-				   {name:"Harvard Square", coords:{lat:42.373362, lng:-71.118956}},
-				   {name:"Central Square", coords:{lat:42.365486, lng:-71.103802}},
-				   {name:"Kendall/MIT", coords:{lat:42.36249079, lng:-71.08617653}},
-				   {name:"Charles/MGH", coords:{lat:42.361166, lng:-71.070628}},
-				   {name:"Park Street", coords:{lat:42.35639457, lng:-71.0624242}},
-				   {name:"Downtown Crossing", coords:{lat:42.355518, lng:-71.060225}},
-				   {name:"South Station", coords:{lat:42.352271, lng:-71.05524200000001}},
-				   {name:"Broadway", coords:{lat:42.342622, lng:-71.056967}},
-				   {name:"Andrew", coords:{lat:42.330154, lng:-71.057655}},
-				   {name:"JFK/UMass", coords:{lat:42.320685, lng:-71.052391}},
-				   {name:"Savin Hill", coords:{lat:42.31129, lng:-71.053331}},
-				   {name:"Fields Corner", coords:{lat:42.300093, lng:-71.061667}},
-				   {name:"Shawmut", coords:{lat:42.29312583, lng:-71.06573796000001}},
-				   {name:"Ashmont", coords:{lat:42.284652, lng:-71.06448899999999}},
-				   {name:"North Quincy", coords:{lat:42.275275, lng:-71.029583}},
-				   {name:"Wollaston", coords:{lat:42.2665139, lng:-71.0203369}},
-				   {name:"Quincy Center", coords:{lat:42.251809, lng:-71.005409}},
-				   {name:"Quincy Adams", coords:{lat:42.233391, lng:-71.007153}},
-				   {name:"Braintree", coords:{lat:42.2078543, lng:-71.0011385}}]
-var connectedStations = [[0, 1],
-						[1, 2],
-						[2, 3],
-						[3, 4],
-						[4, 5],
-						[5, 6],
-						[6, 7],
-						[7, 8],
-						[8, 9],
-						[9, 10],
-						[10, 11],
-						[11, 12],
-						[12, 13],
-						[13, 14],
-						[14, 15],
-						[15, 16],
-						[17, 18],
-						[18, 19],
-						[19, 20],
-						[20, 21],
-						[12, 17]];
+var markerList = [];
+var stations = {
+				   "Alewife":{coords:{lat:42.395428, lng:-71.142483}, connectedTo:["Davis"]},
+				   "Davis":{coords:{lat:42.39674, lng:-71.121815}, connectedTo:["Porter Square"]},
+				   "Porter Square":{coords:{lat:42.3884, lng:-71.11914899999999}, connectedTo:["Harvard Square"]},
+				   "Harvard Square":{coords:{lat:42.373362, lng:-71.118956}, connectedTo:["Central Square"]},
+				   "Central Square":{coords:{lat:42.365486, lng:-71.103802}, connectedTo:["Kendall/MIT"]},
+				   "Kendall/MIT":{coords:{lat:42.36249079, lng:-71.08617653}, connectedTo:["Charles/MGH"]},
+				   "Charles/MGH":{coords:{lat:42.361166, lng:-71.070628}, connectedTo:["Park Street"]},
+				   "Park Street":{coords:{lat:42.35639457, lng:-71.0624242}, connectedTo:["Downtown Crossing"]},
+				   "Downtown Crossing":{coords:{lat:42.355518, lng:-71.060225}, connectedTo:["South Station"]},
+				   "South Station":{coords:{lat:42.352271, lng:-71.05524200000001}, connectedTo:["Broadway"]},
+				   "Broadway":{coords:{lat:42.342622, lng:-71.056967}, connectedTo:["Andrew"]},
+				   "Andrew":{coords:{lat:42.330154, lng:-71.057655}, connectedTo:["JFK/UMass"]},
+				   "JFK/UMass":{coords:{lat:42.320685, lng:-71.052391}, connectedTo:["Savin Hill", "North Quincy"]},
+				   "Savin Hill":{coords:{lat:42.31129, lng:-71.053331}, connectedTo:["Fields Corner"]},
+				   "Fields Corner":{coords:{lat:42.300093, lng:-71.061667}, connectedTo:["Shawmut"]},
+				   "Shawmut":{coords:{lat:42.29312583, lng:-71.06573796000001}, connectedTo:["Ashmont"]},
+				   "Ashmont":{coords:{lat:42.284652, lng:-71.06448899999999}, connectedTo:[]},
+				   "North Quincy":{coords:{lat:42.275275, lng:-71.029583}, connectedTo:["Wollaston"]},
+				   "Wollaston":{coords:{lat:42.2665139, lng:-71.0203369}, connectedTo:["Quincy Center"]},
+				   "Quincy Center":{coords:{lat:42.251809, lng:-71.005409}, connectedTo:["Quincy Adams"]},
+				   "Quincy Adams":{coords:{lat:42.233391, lng:-71.007153}, connectedTo:["Braintree"]},
+				   "Braintree":{coords:{lat:42.2078543, lng:-71.0011385}, connectedTo:[]}
+				}
 
 function init() {
 	map = new google.maps.Map(document.getElementById('map'), {
@@ -51,8 +33,7 @@ function init() {
           	zoom: 13
         });
 
-	populateStationMarkers();
-	connectStationMarkers();
+	populateStations();
 	getUserLocation();
 }
 
@@ -77,49 +58,102 @@ function handleUserLocation() {
 }
 
 function populateUserMarker() {
-	userMarker = new google.maps.Marker({
-    	position: userPos,
-    	icon: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png",
-    	map: map
-  	});
+	placeMarker(userPos, "You are here", "http://maps.google.com/mapfiles/ms/icons/blue-dot.png");
   	map.panTo(userPos);
 }
 
 function calculateClosestStation() {
+	var shortestDistance = -1;
+	var closestStation;
 
+	for (var stationName in stations) {
+		var distance = haversine_distance(userPos, stations[stationName].coords)
+		stations[stationName]["distance"] = distance;
+		if (shortestDistance == -1 || distance < shortestDistance) {
+			shortestDistance = distance;
+			closestStation = stationName;
+		}
+	}
+	var polylineCoords = [stations[closestStation].coords, userPos];
+	drawPath('#0000FF', polylineCoords);
 }
 
 function populateStationMarkers() {
-	var marker;
 	for (var i = 0; i < stationList.length; i++) {
-		station = stationList[i]
-		marker = new google.maps.Marker({
-	    	position: station.coords,
-	    	map: map,
-	    	title: station.name,
-	    	icon: "mbta.png"
-	  	});
+		station = stationList[i];
+		
+	}
+}
+
+function populateStations() {
+	var polylineCoords = [];
+	var station;
+	for (var stationName in stations) {
+		station = stations[stationName];
+		placeMarker(station.coords, stationName, "mbta.png");
+
+		polylineCoords[0] = station.coords;
+		for (var i = 0; i < station.connectedTo.length; i++) {
+			polylineCoords[1] = stations[station.connectedTo[i]].coords;
+			drawPath('#FF0000', polylineCoords);
+		}
 	}
 }
 
 function connectStationMarkers() {
-	/* Connects alewife to ashmont */
-	var polylineCoords = [];
+	
 	var path;
 	var connectedIndicies;
 
 	for (var i = 0; i < connectedStations.length; i++) {
 		connectedIndicies = connectedStations[i];
-		polylineCoords[0] = stationList[connectedIndicies[0]].coords;
-		polylineCoords[1] = stationList[connectedIndicies[1]].coords;
-
-		path = new google.maps.Polyline({
-		    path: polylineCoords,
-		    geodesic: true,
-		    strokeColor: '#FF0000',
-		    strokeOpacity: 1.0,
-		    strokeWeight: 2,
-		    map: map
-	  	});
+		
 	}
+}
+
+/* helper function, draws path of specified color between polylineCoords */
+function drawPath(color, polylineCoords) {
+	var path = new google.maps.Polyline({
+	    path: polylineCoords,
+	    geodesic: true,
+	    strokeColor: color,
+	    strokeOpacity: 1.0,
+	    strokeWeight: 2,
+	    map: map
+  	});
+}
+
+/* helper function, places marker at coords */
+function placeMarker(coords, title, icon) {
+	var marker = new google.maps.Marker({
+    	position: coords,
+    	map: map,
+    	title: title,
+    	icon: icon
+  	});
+  	google.maps.event.addListener(marker, 'click', function() { 
+   		 markerClicked(marker);
+	});
+}
+
+function markerClicked(marker) {
+	alert(marker.title);
+}
+
+/* modified from http://stackoverflow.com/a/14561433 */
+Number.prototype.toRad = function() {
+   return this * Math.PI / 180;
+}
+
+function haversine_distance(coords1, coords2) {
+	var R = 6371; // km 
+	var dLat = (coords2.lat - coords1.lat).toRad();
+	var dLon = (coords2.lng - coords1.lng).toRad();
+	var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + 
+            Math.cos(coords1.lat.toRad()) * Math.cos(coords2.lat.toRad()) * 
+            Math.sin(dLon / 2) * Math.sin(dLon / 2);  
+	var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a)); 
+	var d = R * c; 
+
+	return d;
 }
